e6998c0288ccc7449d15c17b787f600c5eb971f2
Cristopher Belmontes <18002144@alumnos.utleon.edu.mx>
2023-03-08 08:08:38 -0600

    Inventario funcion

--- Assets/script/Inventory.cs
+++ Assets/script/Inventory.cs
@@ -23,6 +23,11 @@ public class Inventory : MonoBehaviour
         for (int i = 0; i < allSlots; i++)
         {
             slot[i]= slotHolder.transform.GetChild(i).gameObject;
+
+            if (slot[i].GetComponent<Slot>().item==null)
+            {
+                slot[i].GetComponent<Slot>().empty = true;
+            }
         }
     }
 
@@ -41,5 +46,48 @@ public class Inventory : MonoBehaviour
         {
             inventory.SetActive(false);
         }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag=="Item")
+        {
+            GameObject itemPickedUp = other.gameObject;
+
+            Item item = itemPickedUp.GetComponent<Item>();
+
+            AddItem(itemPickedUp,item.ID,item.type,item.descripcion,item.icon);
+        }
+    }
+
+    public void AddItem(GameObject itemObject, int itemID, string itemType, string itemDescription, Sprite itemIcon)
+    {
+        for (int i = 0; i < allSlots; i++)
+        {
+            if (slot[i].GetComponent<Slot>().empty)
+            {
+                itemObject.GetComponent<Item>().pickedUp = true;
+
+                slot[i].GetComponent<Slot>().item = itemObject;
+                slot[i].GetComponent<Slot>().ID = itemID;
+                slot[i].GetComponent<Slot>().type = itemType;
+                slot[i].GetComponent<Slot>().description = itemDescription;
+                slot[i].GetComponent<Slot>().icon = itemIcon;
+
+                itemObject.transform.parent = slot[i].transform;
+                itemObject.SetActive(false);
+
+
+                slot[i].GetComponent<Slot>().UpdateSlot();
+
+
+                slot[i].GetComponent<Slot>().empty = false;
+
+                return;
+            }
+            
+
+        }
+
     }
 }

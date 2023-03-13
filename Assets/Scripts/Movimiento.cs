2f05147fe236ffa2a9dace0d42af31eb1c1b3b4a
MarioAlonsoChavez <mario.alonso.chavez.s@gmail.com>
2023-03-12 21:51:15 -0600

    Mejora en animación
    Se mejoró la caminata del personaje femenino y masculino

---
+++ Assets/Scripts/Movimiento.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+   private PlayerController player;
+    private Animator anim;
+    private float velocidad = 2f;
+    private float inputHorizontal;
+    private float rotacionPersonaje;
+    private string direccion = "Derecha";
+    private CharacterController controladorPersonaje;
+    private Vector3 movimiento;
+   
+    void Start()
+    {
+        anim = this.GetComponent<Animator>();
+        controladorPersonaje = this.GetComponent<CharacterController>();
+        Application.targetFrameRate = 60;
+
+    }
+
+    void Update()
+    {
+        MoverPersonaje();
+    }
+
+    void MoverPersonaje()
+    {
+        inputHorizontal = Input.GetAxisRaw("Horizontal");
+        movimiento.z = inputHorizontal * velocidad;
+        if(inputHorizontal > 0)
+        {
+            if(direccion == "Izquierda")
+            {
+                rotacionPersonaje = -180f;
+                this.transform.Rotate(Vector3.up,rotacionPersonaje);
+                direccion = "Derecha";
+            }
+            anim.SetBool(name:"Caminando",value:true);
+            controladorPersonaje.SimpleMove(movimiento);
+            return;
+        }
+
+        if(inputHorizontal < 0)
+        {
+            if(direccion == "Derecha")
+            {
+                rotacionPersonaje = 180;
+                this.transform.Rotate(Vector3.up,rotacionPersonaje);
+                direccion = "Izquierda";
+            }
+            anim.SetBool(name:"Caminando",value:true);
+            controladorPersonaje.SimpleMove(movimiento);
+            return;
+        }
+        anim.SetBool(name:"Caminando",value:false);
+    }
+}

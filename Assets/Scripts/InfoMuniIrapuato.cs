a406aff9a727af628aab334f5cdcfcd560dc2945
MarioAlonsoChavez <mario.alonso.chavez.s@gmail.com>
2023-04-10 21:54:11 -0600

    Botón de información
    Se añadió el botón de información en los retos del nivel 4.

---
+++ Assets/Scripts/InfoMuniIrapuato.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InfoMuniIrapuato : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Metodo que se encarga de llamar despues de cierto tiempo 
+        Invoke("WaitToEnd", 40);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Preguntar si se preciona la tecla de escape y mandara a la escena principal
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("Reto 1.4");
+        }
+
+    }
+
+    //Creaci�n de metodo para despues de ciertos segundos pasemos al MainMenu
+    public void WaitToEnd()
+    {
+        SceneManager.LoadScene("Reto 1.4");
+
+    }
+}
